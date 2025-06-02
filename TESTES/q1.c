032320e76a1626505b6c27ea8b55d8c6ad840b6d
João Lucas de Vilas Bôas Faria <148912535+JoaoLucasFaria@users.noreply.github.com>
2025-06-01 23:17:29 -0300

    Update q1.c

--- TESTES/q1.c
+++ TESTES/q1.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
-
-int main(){
-
- int choice = 1, num, soma = 0;
-
-while (choice)
-    {
-        printf("Escolha uma número (ou digite 0 para sair)\n\n");
-        scanf("%d", &num);
-
-        if(num == 0) choice = 0;
-
-        if (num %2 != 0)
-        {
-            soma += num;
-        }
-
-
-    }    
-
-    printf("Soma: %d\n\n", soma);
-
+int main() {
+    int a = 10;
+    float b = 2.5;
+    return 0;
 }
